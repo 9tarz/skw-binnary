eee05fa8b7649360e664cdcd0bc48d3c7234399b
9tarz <kanatip.jit@gmail.com>
2013-10-25 01:19:14 +0700

    Updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def new
 	@user = User.new
-  end
-
+  end	
   def create
-	@user = User.new(params[:user])
+	@user = User.create(params[:user])
 	if @user.save
 		session[:user_id] = @user.id
 		redirect_to root_url
